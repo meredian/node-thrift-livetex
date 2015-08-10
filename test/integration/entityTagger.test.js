3eef99d687c3db2391c337e2fd618dde1f8b2009
Anton Sidelnikov <ndmeredian@gmail.com>
2015-08-10 18:48:26 +0600

    Add exceptions to EntityTagger service

--- test/integration/entityTagger.test.js
+++ test/integration/entityTagger.test.js
@@ -1,4 +1,6 @@
 import {client} from './helper';
+import {IncorrectEntity} from '../../lib/gen-nodejs/entityService_types'
+import {IncorrectTag} from '../../lib/gen-nodejs/tagService_types'
 
 describe('EntityTagger', function() {
     beforeEach(async function() {
@@ -13,6 +15,20 @@ describe('EntityTagger', function() {
             let fetchedEntity = await client.getEntity(this.entity.id);
             fetchedEntity.tags.should.include(this.newTag.id);
         });
+
+        it('does nothing if tag already linked to entity', async function() {
+            await client.addEntityTag(this.entity.id, this.linkedTag.id);
+            let fetchedEntity = await client.getEntity(this.entity.id);
+            fetchedEntity.tags.should.include(this.linkedTag.id);
+        });
+
+        it('throws IncorrectTag on incorrect tag id', async function() {
+            return client.addEntityTag(this.entity.id, -1).should.be.rejectedWith(IncorrectTag);
+        });
+
+        it('throws IncorrectEntity on incorrect entity id', async function() {
+            return client.addEntityTag(-1, this.newTag.id).should.be.rejectedWith(IncorrectEntity);
+        });
     });
 
     describe('#removeEntityTag', function() {
@@ -21,6 +37,20 @@ describe('EntityTagger', function() {
             let fetchedEntity = await client.getEntity(this.entity.id);
             fetchedEntity.tags.should.not.include(this.linkedTag.id);
         });
+
+        it('does nothing if tag already not linked to entity', async function() {
+            await client.removeEntityTag(this.entity.id, this.newTag.id);
+            let fetchedEntity = await client.getEntity(this.entity.id);
+            fetchedEntity.tags.should.not.include(this.newTag.id);
+        });
+
+        it('throws IncorrectTag on incorrect tag id', async function() {
+            return client.removeEntityTag(this.entity.id, -1).should.be.rejectedWith(IncorrectTag);
+        });
+
+        it('throws IncorrectEntity on incorrect entity id', async function() {
+            return client.removeEntityTag(-1, this.linkedTag.id).should.be.rejectedWith(IncorrectEntity);
+        });
     });
 
     describe('#getEntityTags', function() {
@@ -28,12 +58,20 @@ describe('EntityTagger', function() {
             let tags = await client.getEntityTags(this.entity.id);
             tags.should.deep.equal([this.linkedTag]);
         });
+
+        it('throws IncorrectEntity on incorrect entity id', async function() {
+            return client.getEntityTags(-1).should.be.rejectedWith(IncorrectEntity);
+        });
     });
 
     describe('#getEntitiesByTagList', function() {
         it('returns list of entities by tag id list', async function() {
             let entities = await client.getEntitiesByTagList([this.linkedTag.id]);
             entities.should.deep.equal([this.entity]);
+        });
+
+        it('throws IncorrectTag on incorrect tag id in list', async function() {
+            return client.getEntitiesByTagList([-1]).should.be.rejectedWith(IncorrectTag);
         });
     });
 });
