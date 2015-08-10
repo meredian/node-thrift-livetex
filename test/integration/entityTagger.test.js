7e393bfcd9b58fa1c9dea94971c2befa6073a44a
Anton Sidelnikov <ndmeredian@gmail.com>
2015-08-10 14:28:19 +0600

    Implement EntityTagger service

--- test/integration/entityTagger.test.js
+++ test/integration/entityTagger.test.js
@@ -1,15 +1,39 @@
 import {client} from './helper';
 
 describe('EntityTagger', function() {
+    beforeEach(async function() {
+        this.linkedTag = await client.createTag({name: "linked_tag"});
+        this.newTag = await client.createTag({name: "new_tag"});
+        this.entity = await client.createEntity({name: "random_entity", tags: [this.linkedTag.id]});
+    });
+
     describe('#addEntityTag', function() {
-        xit('adds existing tag to existing entity', async function() {
-            this.tag = await client.createTag({name: "random_tag"});
-            this.entity = await client.createEntity({name: "random_entity"});
+        it('adds existing tag to existing entity', async function() {
+            await client.addEntityTag(this.entity.id, this.newTag.id);
+            let fetchedEntity = await client.getEntity(this.entity.id);
+            fetchedEntity.tags.should.include(this.newTag.id);
+        });
+    });
 
-            console.log("Tag: %j, Entity: %j", this.tag, this.entity);
+    describe('#removeEntityTag', function() {
+        it('removes existing tag from existing entity with this tag', async function() {
+            await client.removeEntityTag(this.entity.id, this.linkedTag.id);
+            let fetchedEntity = await client.getEntity(this.entity.id);
+            fetchedEntity.tags.should.not.include(this.linkedTag.id);
+        });
+    });
 
-            client.addEntityTag(this.entity.id, this.tag.id);
-            client.getEntity(this.entity.id).tags.should.inclue(tag.id);
+    describe('#getEntityTags', function() {
+        it('returns list of tags for entity', async function() {
+            let tags = await client.getEntityTags(this.entity.id);
+            tags.should.deep.equal([this.linkedTag]);
+        });
+    });
+
+    describe('#getEntitiesByTagList', function() {
+        it('returns list of entities by tag id list', async function() {
+            let entities = await client.getEntitiesByTagList([this.linkedTag.id]);
+            entities.should.deep.equal([this.entity]);
         });
     });
 });
